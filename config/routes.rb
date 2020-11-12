5861cc43360b31386af71f63be30636f58738ed5
Alain Rusnov <alainrusnov@gmail.com>
2020-11-12 14:43:14 -0500

    Finished backend for MrCocktail. works

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   resources :cocktails do
     resources :doses, only: [:new, :create]
   end
+  resources :doses, only: :destroy  # put destroy outside of nested loop -> because ends up being coktails/cocktail_id/doses/:id and not /doses/:id
 end
